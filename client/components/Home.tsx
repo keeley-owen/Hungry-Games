d80488e26d83c4c6ac5b59707b75b7db94e57531
lj-linessmith <142572937+lj-linessmith@users.noreply.github.com>
2023-11-30 16:13:02 +1300

    started work on getCurrentLocation functions + API implementation

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,3 +1,82 @@
-export default function Home(){
-  
+import React, { useState } from 'react'
+import { getCurrentLocationApi } from '../apis/locationApi'
+import { useQuery } from '@tanstack/react-query'
+import { Location } from '../../models/fruit'
+
+const initialFormData = {
+  address: '',
+}
+
+export default function Home() {
+  const {
+    data: currentLocation,
+    error,
+    isLoading,
+  } = useQuery({ queryKey: [], queryFn: getCurrentLocationApi })
+
+  const [formData, setFormData] = useState(initialFormData)
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setFormData({
+      ...formData,
+      [name]: value,
+    })
+  }
+
+  if (error) {
+    console.log(error)
+  }
+
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+
+  const latitude = currentLocation.lat
+  const longitude = currentLocation.lng
+
+  //dispalys coordinates onscreen for testing purposes
+  function displayLocation() {
+    //checks that coordinates are being returned from api
+    if (latitude != null && longitude != null) {
+      return (
+        <>
+          <p>Current Location: </p>
+          <p>
+            {' '}
+            {latitude} {longitude}{' '}
+          </p>
+        </>
+      )
+    } else {
+      return <p>Location is sad {':('} </p>
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const startCharacter = '${'
+    const endCharacter = '}'
+    const searchInput = formData.address.split(' ')
+    const apiQuery = startCharacter + searchInput.join('}%20${') + endCharacter
+    return apiQuery
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="address"
+          value={formData.address}
+          onChange={handleInputChange}
+        />
+      </form>
+
+      <button className="sumbitButton" type="submit">
+        SUBMIT LOCATION
+      </button>
+      {/* {displayLocation()} */}
+    </>
+  )
 }
--- models/fruit.ts
+++ models/fruit.ts
@@ -6,3 +6,20 @@ export interface Fruit {
 export interface FruitData {
   name: string
 }
+
+export interface Result {
+  formattedAddress: string
+  geometry: Geometry
+  placeID: string
+  types: string[]
+}
+export interface Geometry {
+  location: Location
+  locationType: string
+  viewport: Viewport
+}
+
+export interface Location {
+  lat: number
+  lng: number
+}
