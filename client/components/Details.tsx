53ecd2925a5442d90a8e45d05212b8db7393542b
LJ LinesSmith <lewis.lines.smith@gmail.com>
2023-12-03 16:11:20 +1300

    pulled ton's fixes

--- client/components/Details.tsx
+++ client/components/Details.tsx
@@ -1,29 +1,46 @@
-import { useQuery } from "@tanstack/react-query"
-import { getLocationsDetailsApi } from "../apis/details"
+import { useQuery } from '@tanstack/react-query'
+import { getLocationsDetailsApi } from '../apis/details'
+import { useEffect, useState } from 'react'
 
-export default function Details() {
-  const { error, isLoading } = useQuery({
+interface Location {
+  name: string
+}
+
+export default async function Details(listProps) {
+  const [currentLocation, setCurrentLocation] = useState<Location | null>(null)
+  const { data, error, isLoading } = useQuery({
     queryKey: [],
     queryFn: getLocationsDetailsApi,
   })
-  if (error) {
-    console.log(error)
-  }
+
+  useEffect(() => {
+    if (data) {
+      const selectedLocations = listProps
+      const randomValue = Math.floor(Math.random() * selectedLocations.length)
+      const winner = selectedLocations[randomValue]
+      const apiWinner = winner.place_id
+
+      getLocationsDetailsApi(apiWinner)
+        .then((location) => {
+          setCurrentLocation(location)
+        })
+        .catch((error) => {
+          console.error('Error fetching location details', error)
+        })
+    }
+  }, [data, listProps])
 
   if (isLoading) {
     return <p>Loading...</p>
   }
-  const currentLocation = await getLocationsDetailsApi(apiWinner)
-  try {
-    setLatitude(currentLocation.results[0].geometry.location.lat)
-  } catch (error) {
-    console.error('location is sad :(', error)
-    setLatitude(null)
+
+  if (error) {
+    console.log(error)
   }
+
   return (
-
-
-
-
+    <>
+      <div>{currentLocation && <div>{currentLocation.name}</div>}</div>
+    </>
   )
 }
