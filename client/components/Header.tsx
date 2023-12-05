d43edbec7c4cbc2e3570320e0eb5ece38aae34a6
Keeley Owen <keeleytowen@gmail.com>
2023-12-06 12:49:41 +1300

    css changes

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,11 +1,13 @@
 export default function Header() {
   return (
     <>
-      <a href="/">
-        <button className="header" type="reset">
-          <span>the hungry games </span>
-        </button>
-      </a>
+      <div className="headButton">
+        <a href="/">
+          <button className="header" type="reset">
+            <span>the hungry games </span>
+          </button>
+        </a>
+      </div>
     </>
   )
 }
